ad31d56695f9c247a5d23e784d781086e66ab217
Eren Önen <erenoit@protonmail.com>
2023-08-29 16:26:42 +0300

    build: add basic premake5 script

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+workspace "Sudoku"
+  toolset "clang"
+  debugger "gdb"
+  cppdialect "C++20"
+  cdialect "C17"
+  configurations { "Debug", "OptimizedDebug", "Release" }
+  platforms { "Linux", "Windows" }
+  architecture "x86_64"
+
+project "Sudoku"
+  kind "WindowedApp"
+  language "C++"
+  targetdir "bin/%{cfg.buildcfg}"
+
+  externalincludedirs { "lib/glad/include", "lib/glm" }
+
+  links { "glfw" }
+
+  files { "src/**.hpp", "src/**.cpp", "lib/glad/src/glad.c" }
+
+  filter "platforms:Linux"
+      libdirs { "/usr/lib" }
+
+  filter "platforms:Windows"
+      libdirs { "" }
+
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+  filter "configurations:OptimizedDebug"
+    defines { "DEBUG" }
+    optimize "On"
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
